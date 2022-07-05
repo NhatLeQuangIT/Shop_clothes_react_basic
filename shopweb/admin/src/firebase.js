f9dd388904ecfc77b1d20134af484eee71ae9205
Lê Nhật <nhatly11072000@gmail.com>
2022-07-05 19:01:56 +0700

    lan1

---
+++ shopweb/admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBTjijSTxF8dJYLk85BGZJocK1MvwhIYf0",
+    authDomain: "clothes-4c4aa.firebaseapp.com",
+    projectId: "clothes-4c4aa",
+    storageBucket: "clothes-4c4aa.appspot.com",
+    messagingSenderId: "1093892794848",
+    appId: "1:1093892794848:web:fb4ed5358ed2bb51aca385",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ shopweb/client/src/data.js
@@ -0,0 +1,118 @@
+export const sliderItems = [{
+        id: 1,
+        img: "https://www.papercitymag.com/wp-content/uploads/2018/08/Lone_Star_Looking_Glass_Summer_Outfit-1.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/DG69bQ4/2.png",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/cXFnLLV/3.png",
+        title: "LOUNGE WEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [{
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "SHIRT STYLE!",
+        cat: "Women",
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "COAT",
+        cat: "Coat",
+    },
+    {
+        id: 3,
+        img: "https://media.vogue.fr/photos/5d3985538602c4000a1fcb2b/2:3/w_2560%2Cc_limit/180411_CM_ParisVogue_03_0506_R3.jpg",
+        title: "JEANS",
+        cat: "Jean",
+    },
+    {
+        id: 4,
+        img: "https://cf.shopee.vn/file/046a9190a977a50213ffac38750870fa",
+        title: "SHOES",
+        cat: "Shoes",
+    },
+    {
+        id: 5,
+        img: "https://image.made-in-china.com/202f0j00mrUfwDWaaYbq/High-Quality-Fashion-Design-Handbags-for-Women-Ladies-Bag-Shoulder-Bag.jpg",
+        title: "BAG",
+        cat: "Bag",
+    },
+    {
+        id: 6,
+        img: "https://images.unsplash.com/photo-1614252369475-531eba835eb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFzaGlvbiUyMG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        title: "FASHION MAN",
+        cat: "Man",
+    },
+    {
+        id: 7,
+        img: "https://images.unsplash.com/photo-1607807722648-2b1ecf58a104?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fGZhc3NoaW9uJTIwaG9vZGllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        title: "HOODIE",
+        cat: "Hoodie",
+    },
+    {
+        id: 8,
+        img: "https://images.unsplash.com/photo-1611312449408-fcece27cdbb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8amFja2V0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        title: "LIGHT JACKETS",
+        cat: "Jacket",
+    },
+    {
+        id: 9,
+        img: "https://images.unsplash.com/photo-1583744946564-b52ac1c389c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dCUyMHNoaXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        title: "T-SHIRTS",
+        cat: "T-shirt",
+    },
+    {
+        id: 10,
+        img: "https://images.unsplash.com/photo-1550639525-c97d455acf70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZHJlc3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        title: "DRESS",
+        cat: "Dress",
+    },
+];
+
+export const popularProducts = [{
+        id: 1,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+        id: 2,
+        img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+        id: 3,
+        img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+        id: 4,
+        img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+        id: 5,
+        img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+        id: 6,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+        id: 7,
+        img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+        id: 8,
+        img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+];
